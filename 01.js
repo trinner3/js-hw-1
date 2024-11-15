c710a52d54431d8032e66545d7b050275c5a2482
trinner3 <164670497+trinner3@users.noreply.github.com>
2024-11-15 19:22:19 +0300

    01.js

--- 01.js
+++ 01.js
@@ -1,13 +1,11 @@
 // С помощью цикла while найдите сумму чисел от 1 до 20
 // Результат присвойте переменной sum
-const MIN = 1
-const MAX = 20
-let x = MIN
-let y = 1
-while (x < MAX) {
-    x = x + 1
-    y = y + x
-}
+
 // your code
-let sum = y
+let x = 1
+let sum = 0
+while (x <= 20) {
+    sum += x
+    x++
+} 
 console.log (sum)
